01ea80fd4518f5fcaa5a5b6e7694b1ce9831b474
Kevin McAleer <kevinmcaleer@me.com>
2019-09-14 19:45:23 +0100

    added range finder

--- Arduino/bluetooth_control/bluetooth_control.ino
+++ Arduino/bluetooth_control/bluetooth_control.ino
@@ -4,6 +4,7 @@
 // May 2019 - added buzzer feature
 // Requires a Fundomoto sheild
 // September 2019 - use software serial for bluetooth module, using builtin rx/tx with bluetooth causes garbage on rx
+// http://rc.mired.org/2017/09/the-fundumoto-motor-shield.html
 
 //const int left_motor_direction = 13;
 //const int right_motor_direction = 12;
@@ -18,7 +19,7 @@
 //const int right_motor_IR_sensor_pin = 3;
 
 #include "SoftwareSerial.h"
-SoftwareSerial MyBlue(2, 3); // RX | TX 
+SoftwareSerial MyBlue(2, 3); // RX | TX
 
 //int ch_A_Brake = 9;
 //int ch_B_Brake = 8;
@@ -29,16 +30,26 @@ int ch_B_speed = 11;
 char state = 0;
 int delaylength = 1000;
 int buzzerPin = 4;
+
+int trigPin = 7;    // Trigger
+int echoPin = 8;    // Echo
+long duration, cm, inches;
+
 //#define BUFFER_SIZE = 64; // this will prevent buffer overruns
 //char inData[BUFFER_SIZE]; // this is a character buffer
 char inChar=-1; // initalise the first character as nothing
 
 void setup() {
+
+  // setup ultrasonic rangefinder
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
   // put your setup code here, to run once:
   Serial.begin(9600, SERIAL_8N1);
   Serial.println("SMARSFan OS 1.3");
   Serial.println("---------------");
-  MyBlue.begin(9600);  //Baud Rate for AT-command Mode.  
+  MyBlue.begin(9600);  //Baud Rate for AT-command Mode.
 
   // establish motor direction toggle pins
   pinMode(ch_A_Direction, OUTPUT);
@@ -52,6 +63,35 @@ void setup() {
 //   buzz();
 //   delay(delaylength/10);
 //   buzz();
+}
+
+void rangefinder() {
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  // inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+
+  // Serial.print(inches);
+  // Serial.print("in, ");
+  MyBlue.print(cm);
+  MyBlue.print("cm");
+  MyBlue.println();
+//  Serial.println();
+
+  delay(250);
 }
 
 void buzz(){
@@ -75,7 +115,6 @@ void forward() {
 
   analogWrite(ch_A_speed, 0); // stop
   analogWrite(ch_B_speed, 0); // stop
-
 
   //  Serial.flush();
 }
@@ -150,7 +189,7 @@ void loop()
   bool isData = false;
   inChar = "";
   // from bluetooth to terminal
-  if (MyBlue.available()) 
+  if (MyBlue.available())
   {
     inChar = MyBlue.read();
     Serial.write(MyBlue);
@@ -170,30 +209,33 @@ void loop()
 //      Serial.print(value);
 //      Serial.print(" Character:");
 //      Serial.println(inChar);
-      if (inChar == 'u') 
+      if (inChar == 'u')
       {
         forward();
       }
-      else if (inChar == 'd') 
+      else if (inChar == 'd')
       {
         backward();
       }
-      else if (inChar == 'l') 
+      else if (inChar == 'l')
       {
         left();
       }
-      else if (inChar == 'r') 
+      else if (inChar == 'r')
       {
         right();
       }
-      else if (inChar == "s") 
+      else if (inChar == "s")
       {
         fullstop();
       }
-      else if (inChar == 'b') 
+      else if (inChar == 'b')
       {
         buzz();
       }
+      else if (inChar == 'u')
+      {
+        rangefinder();
+      }
   }
 }
- 
--- butil.py
+++ butil.py
@@ -18,8 +18,9 @@ CONNECTED = False
 print("Start")
 try:
     PORT = "/dev/tty.SMARS-SPPDev"
-    BLUETOOTH = serial.Serial(PORT, 9600, timeout = 3,
-     xonxoff=False, parity=serial.PARITY_NONE, stopbits=1)
+    # BLUETOOTH = serial.Serial(PORT, 9600, timeout = 3,
+    #  xonxoff=False, parity=serial.PARITY_NONE, stopbits=1)
+    BLUETOOTH = serial.Serial(PORT, 9600, parity=serial.PARITY_NONE, stopbits=1)
     time.sleep(DELAY)
     print (BLUETOOTH)
     print("connected")
@@ -30,10 +31,11 @@ except:
 if CONNECTED:
     NB = "1"
     while NB != "q":
+        time.sleep(0.1)
         NB = ""
         NB = input('WASD: ')
         NB = NB.strip()
-        print("Got: ", NB)
+        # print("Got: ", NB)
         if NB == "w":
             send_up()
         if NB == "s":
@@ -42,5 +44,13 @@ if CONNECTED:
             BLUETOOTH.write(bytes(b'l')) # left
         if NB == "d":
             BLUETOOTH.write(bytes(b'r')) # right
-        if NB == 'b':
-            BLUETOOTH.write(bytes(b'b'))
+        if NB == "b":
+            BLUETOOTH.write(bytes(b'b')) # buzz
+        if NB == "u":
+            print("got U")
+            BLUETOOTH.write(bytes(b'u')) # rangefind
+            time.sleep(0.1)
+
+            # distance = ""
+            distance = BLUETOOTH.readline() # get distance in cm
+            print(f"distance is: {distance}")
